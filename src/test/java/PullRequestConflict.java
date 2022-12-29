3839eda34fbe8519941d863f8d6131531d225661
ElenaQAE <fimushkin.dmitry@yandex.ru>
2022-12-29 11:09:48 +0300

    init commit

---
+++ src/test/java/PullRequestConflict.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class PullRequestConflict {
+    @Test
+    void pullRequestConflict() {
+        open("https://github.com/");
+
+    }
+}
