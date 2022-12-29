591ade8275a59847539c3189856002f17113d15b
ElenaQAE <fimushkin.dmitry@yandex.ru>
2022-12-29 11:12:25 +0300

    url changes

--- src/test/java/PullRequestConflict.java
+++ src/test/java/PullRequestConflict.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class PullRequestConflict {
     @Test
     void pullRequestConflict() {
-        open("https://github.com/");
+        open("http://github.com/");
 
     }
 }
